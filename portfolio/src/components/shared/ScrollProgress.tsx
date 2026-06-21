781405c1c49cf5f73442db01216a41c401f8326a
Ibrahim-sys-ux <ibrahimkutty.c3.11@gmail.com>
2026-06-21 19:28:45 +0530

    Delete portfolio directory

--- portfolio/src/components/shared/ScrollProgress.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-// Scroll progress is handled in Navbar via DOM manipulation for performance
-// This component is intentionally minimal
-export default function ScrollProgress() {
-  return null
-}
