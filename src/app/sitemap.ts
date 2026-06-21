b19259b556dd59866f50ed68cedada76cf2b798f
Ibrahim-sys-ux <ibrahimkutty.c3.11@gmail.com>
2026-06-21 20:20:12 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,11 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://ibrahimkutty.dev',
+      url: 'https://portfolio-5w9.pages.dev',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
