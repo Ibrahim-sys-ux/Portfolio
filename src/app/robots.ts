cb6118a71b41019886e4b6799a0ec8da16f921a5
Ibrahim-sys-ux <ibrahimkutty.c3.11@gmail.com>
2026-06-21 20:19:42 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,13 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://ibrahimkutty.dev/sitemap.xml',
+    sitemap: 'https://portfolio-5w9.pages.dev/sitemap.xml',
   }
 }
